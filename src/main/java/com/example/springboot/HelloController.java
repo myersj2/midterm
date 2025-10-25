1285f3ecaf5b595fc314b9bfcaf2ee1c843b27d2
myersj2 <myersj2@montclair.edu>
2025-10-25 16:43:50 -0400

    fixed hellocontroller

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Hello from Spring Boot in Docker!";
+		return "Greetings from Spring Boot!";
 	}
 
 	@GetMapping("/health")
